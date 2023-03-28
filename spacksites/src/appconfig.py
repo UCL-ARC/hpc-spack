edef9b0072f4711d607128b2d71e73f157503bfe
James Legg <j.legg@ucl.ac.uk>
2023-03-28 13:29:46 +0100

    refactor .ini path subsitution of {hpc_spack_root}

--- spacksites/src/appconfig.py
+++ spacksites/src/appconfig.py
@@ -16,39 +16,24 @@ class AppConfig():
         config.sections()
         config.read(self.ini_file)
         self.config = config
-        sites_root_setting = config['general']['sites_root']
-        if '{hpc_spack_root}' in sites_root_setting:
-            sites_root_setting = sites_root_setting.format(hpc_spack_root=os.path.dirname(spacksites_dir()))
+        sites_root_setting = self._subst_setting(config['general']['sites_root'])
         self.spack_sites_root = os.path.abspath(sites_root_setting)
         self.spack_version = config['general']['spack_version']
         # TODO add the other scipts in process-env-scripts if they get used
-        self.spdsper_script = config['process_env_scripts']['spdsper']
-        self.spd_script = config['process_env_scripts'][spd_setting_key()]
+        self.spdsper_script = self._subst_setting(config['process_env_scripts']['spdsper'])
+        self.spd_script = self._subst_setting(config['process_env_scripts'][spd_setting_key()])
 
-        self.initial_site_config_yaml = config['initial_site_configs']['config_default']
-        if '{hpc_spack_root}' in self.initial_site_config_yaml:
-            self.initial_site_config_yaml = self.initial_site_config_yaml.format(hpc_spack_root=os.path.dirname(spacksites_dir()))
+        self.initial_site_config_yaml = self._subst_setting(config['initial_site_configs']['config_default'])
+        self.initial_site_modules_yaml = self._subst_setting(config['initial_site_configs']['modules_default'])
+        self.initial_site_repos_yaml = self._subst_setting(config['initial_site_configs']['repos_default'])
+        self.initial_site_mirrors_yaml = self._subst_setting(config['initial_site_configs']['mirrors_default'])
+        self.initial_site_packages_yaml = self._subst_setting(config['initial_site_configs'][packages_setting_key()])
+        self.templates_active_set = self._subst_setting(config['spack_env_templates']['active_set'])
+        self.site_archive_root = self._subst_setting(config['spack_env_templates']['archive_root'])
 
-        self.initial_site_modules_yaml = config['initial_site_configs']['modules_default']
-        if '{hpc_spack_root}' in self.initial_site_modules_yaml:
-            self.initial_site_modules_yaml = self.initial_site_modules_yaml.format(hpc_spack_root=os.path.dirname(spacksites_dir()))
-
-        self.initial_site_repos_yaml = config['initial_site_configs']['repos_default']
-        if '{hpc_spack_root}' in self.initial_site_repos_yaml:
-            self.initial_site_repos_yaml = self.initial_site_repos_yaml.format(hpc_spack_root=os.path.dirname(spacksites_dir()))
-
-        self.initial_site_mirrors_yaml = config['initial_site_configs']['mirrors_default']
-        if '{hpc_spack_root}' in self.initial_site_mirrors_yaml:
-            self.initial_site_mirrors_yaml = self.initial_site_mirrors_yaml.format(hpc_spack_root=os.path.dirname(spacksites_dir()))
-
-        self.initial_site_packages_yaml = config['initial_site_configs'][packages_setting_key()]
-        if '{hpc_spack_root}' in self.initial_site_packages_yaml:
-            self.initial_site_packages_yaml = self.initial_site_packages_yaml.format(hpc_spack_root=os.path.dirname(spacksites_dir()))
-
-        self.templates_active_set = config['spack_env_templates']['active_set']
-        if '{hpc_spack_root}' in self.templates_active_set:
-            self.templates_active_set = self.templates_active_set.format(hpc_spack_root=os.path.dirname(spacksites_dir()))
-
-        self.site_archive_root = config['spack_env_templates']['archive_root']
-        if '{hpc_spack_root}' in self.site_archive_root:
-            self.site_archive_root = self.site_archive_root.format(hpc_spack_root=os.path.dirname(spacksites_dir()))
+    def _subst_setting(self, raw_setting):
+        if '{hpc_spack_root}' in raw_setting:
+            return raw_setting.format(hpc_spack_root=os.path.dirname(spacksites_dir()))
+        else:
+            return raw_setting
+        # could add here any further subsitutions that arise
