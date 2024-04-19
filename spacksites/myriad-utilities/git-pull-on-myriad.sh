9a19352dee2836dbdd8476d169133568a190a47b
heatherkellyucl <h.kelly@ucl.ac.uk>
2024-04-19 13:22:27 +0100

    Update to spack 0.21

--- spacksites/myriad-utilities/git-pull-on-myriad.sh
+++ spacksites/myriad-utilities/git-pull-on-myriad.sh
@@ -2,4 +2,4 @@
 # git stash is because spack_sites.ini will have been edited w.r.t to the repo for the root of the spack sites. 
 git stash
 git pull
-sed -i 's%sites_root =.*%sites_root = /lustre/shared/ucl/apps/spack/0.20%' /home/ccspapp/Scratch/spack/0.20/hpc-spack/spacksites/settings/spack_sites.ini
+sed -i 's%sites_root =.*%sites_root = /lustre/shared/ucl/apps/spack/0.21%' /home/ccspapp/Scratch/spack/0.21/hpc-spack/spacksites/settings/spack_sites.ini
