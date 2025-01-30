e460fcc11e02ec2f7063f5e7df4399cdb1c15518
heatherkellyucl <h.kelly@ucl.ac.uk>
2025-01-30 18:01:09 +0000

    No separate devtoolset python or gcc needed on RHEL 9

---
+++ spacksites/myriad-utilities/init-spacksites-rhel9.sh
@@ -0,0 +1,22 @@
+# source this - it sets up shell environment variable
+# this is to use the installation of hpc spack on Myriad as user ccspapp
+
+
+if [[ "$USER" == "ccspapp" ]]; then
+
+    TOP_DIR="${HOME}/Scratch/spack/0.23/hpc-spack"
+    cd "${TOP_DIR}"
+    alias sps="${TOP_DIR}/spacksites/spacksites"
+    pip install --user pyyaml
+    echo "An alias has been set:"
+    echo "       $(alias sps)"
+    echo "This will disappear if you use 'bash' to get another shell, e.g. prior to entering a particular spack site."
+    echo "A dependency, pyyaml, of spacksites has been installed"
+    echo "Do an 'sps list' to test."
+
+else
+
+    echo "This script expects that you are using the installation of spack for user ccspapp"
+    echo "You are not logged in as such - so this script has done nothing"
+
+fi
