53530fba28fc24d02984e9d71366e481484428c3
heatherkellyucl <h.kelly@ucl.ac.uk>
2025-02-05 14:42:55 +0000

    Added rhel 9.5 default setup script and packages yaml. Made .ini use them

---
+++ spacksites/process-env-scripts/spack-deps-rhel-9.5.sh
@@ -0,0 +1,17 @@
+# source this - it is for setting environment variables
+# 
+#
+# A script to set the working environment for spack 
+#
+# The environment set is for
+# (1) Python - spack is written in Python
+# (2) a gcc compiler - spack needs a system compiler at least to bootstrap itself
+#
+# This script is symlinked as spd 
+#
+
+
+# TODO: wonder about how this would take precedence or be obscurced by pre or post loaded modules  
+
+
+
