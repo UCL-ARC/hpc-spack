1709cffca8c9495590dad26606495e24e5c733a8
heatherkellyucl <h.kelly@ucl.ac.uk>
2025-03-17 20:10:06 +0000

    Errant paste in line 3

--- repos/dev/packages/charmpp/package.py
+++ repos/dev/packages/charmpp/package.py
@@ -1,5 +1,5 @@
 # Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
-      - plumed@2.9.2
+#
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
