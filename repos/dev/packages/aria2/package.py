a91ddce7ef03031d4dde9b590d76ba750c43460f
heatherkellyucl <h.kelly@ucl.ac.uk>
2026-03-25 10:51:20 +0000

    Need to keep our aria2 TLS changes, updated imports for 1.1.1

--- repos/dev/packages/aria2/package.py
+++ repos/dev/packages/aria2/package.py
@@ -2,8 +2,11 @@
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
+# UCL: package now has variants and lists gnutls and openssl as conditional dependencies
+#      GnuTLS has precedence by default over OpenSSL if both libraries are installed.
 import sys
+
+from spack_repo.builtin.build_systems.autotools import AutotoolsPackage
 
 from spack.package import *
 
