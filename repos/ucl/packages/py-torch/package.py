7775a6abdb0ba176e8e2de2bb78e56de88421b22
Matthew Gorby <ccaeorb@ucl.ac.uk>
2026-06-08 10:38:09 +0100

    more fixes

--- repos/ucl/packages/py-torch/package.py
+++ repos/ucl/packages/py-torch/package.py
@@ -347,8 +347,8 @@ class PyTorch(PythonPackage, CudaPackage, ROCmPackage):
         depends_on("rocblas")
         depends_on("miopen-hip")
         depends_on("rocminfo")
-        depends_on("aotriton@0.8.1b", when="@2.5:2.6")
-        depends_on("aotriton@0.9.1b", when="@2.7:")
+        #depends_on("aotriton@0.8.1b", when="@2.5:2.6")
+        #depends_on("aotriton@0.9.1b", when="@2.7:")
         depends_on("composable-kernel@:6.3.2", when="@2.5")
         depends_on("composable-kernel@6.3.2:", when="@2.6:")
     depends_on("mpi", when="+mpi")
@@ -717,7 +717,7 @@ class PyTorch(PythonPackage, CudaPackage, ROCmPackage):
             env.set("ROCTRACER_INCLUDE_DIR", self.spec["roctracer-dev"].prefix.include.roctracer)
             if self.spec.satisfies("@2.5:"):
                 env.set("TORCHINDUCTOR_CK_DIR", self.spec["composable-kernel"].prefix)
-                env.set("AOTRITON_INSTALLED_PREFIX", self.spec["aotriton"].prefix)
+                #env.set("AOTRITON_INSTALLED_PREFIX", self.spec["aotriton"].prefix)
             if self.spec.satisfies("^hip@5.2.0:"):
                 env.set("CMAKE_MODULE_PATH", self.spec["hip"].prefix.lib.cmake.hip)
 
