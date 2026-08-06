6417d1edbc2b64e631a2e160e3af10c3b7dc86aa
Kai <126531584+kaibinary@users.noreply.github.com>
2026-08-06 12:25:54 +0100

    Backport conflict with ^wannier90@3:+mpi

--- repos/ucl/packages/vasp/package.py
+++ repos/ucl/packages/vasp/package.py
@@ -94,6 +94,9 @@ class Vasp(MakefilePackage, CudaPackage):
     depends_on("nccl", when="+cuda")
     depends_on("hdf5+fortran+mpi", when="+hdf5")
     depends_on("wannier90", when="+wannier90")
+    # The +mpi variant of wannier90 3.x does not work with VASP
+    # https://vasp.at/wiki/Makefile.include#Wannier90_(optional)
+    conflicts("^wannier90@3:+mpi", msg="The +mpi variant of wannier90 3.x does not work with VASP")
     # at the very least the nvhpc mpi seems required
     depends_on("nvhpc+mpi+lapack+blas", when="%nvhpc")
 
