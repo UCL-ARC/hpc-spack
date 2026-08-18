ec0d226f49d62f8e666f47803521daf028d4438d
Nicole Labra Avila <64498975+NicoleLabrAvila@users.noreply.github.com>
2026-08-18 16:15:58 +0100

    Update package.py

--- repos/ucl/packages/mrtrix3/package.py
+++ repos/ucl/packages/mrtrix3/package.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack_repo.builtin.build_systems.generic import Package
+#from spack_repo.builtin.build_systems.generic import Package
 
 from spack.package import *
 
@@ -51,5 +51,5 @@ class Mrtrix3(Package):
         build()
         install_tree(".", prefix)
 
-    def setup_run_environment(self, env: EnvironmentModifications) -> None:
+    def setup_run_environment(self, env) -> None:
         env.prepend_path("PATH", self.prefix)
